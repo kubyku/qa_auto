efcba6370fad96410a502e31979bd725be0537af
jhk <kjhzxcv@gmail.com>
2026-01-22 19:07:34 +0900

    dotenv

--- web/app.py
+++ web/app.py
@@ -1,3 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
 from __future__ import annotations
 
 import io
